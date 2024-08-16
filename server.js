12f40c3c1aec49bf5fb7f7422c6c6308ee62d53b
Jeff Ripke <jripke74@gmail.com>
2024-08-16 09:49:38 -0700

    add logging code

--- server.js
+++ server.js
@@ -1,21 +1,26 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const path = require("path");
+const path = require('path');
+const { logger } = require('./middleware/logger');
 const PORT = process.env.PORT || 3500;
 
-app.use("/", express.static(path.join(__dirname, "/public")));
+app.use(logger);
 
-app.use("/", require("./routes/root"));
+app.use(express.json());
 
-app.all("*", (req, res) => {
-	res.status(404);
-	if (req.accepts("html")) {
-		res.sendFile(path.join(__dirname, "views", "404.html"));
-	} else if (req.accepts("json")) {
-		res.json({ message: "404 Not Found" });
-	} else {
-		res.type("txt").send("404 Not Found");
-	}
+app.use('/', express.static(path.join(__dirname, 'public')));
+
+app.use('/', require('./routes/root'));
+
+app.all('*', (req, res) => {
+  res.status(404);
+  if (req.accepts('html')) {
+    res.sendFile(path.join(__dirname, 'views', '404.html'));
+  } else if (req.accepts('json')) {
+    res.json({ message: '404 Not Found' });
+  } else {
+    res.type('txt').send('404 Not Found');
+  }
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
